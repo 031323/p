28258d6587a9deb6b88660319c0afafea6c3a05c
ujjwalraajput <ujjwal.raajput@gmail.com>
2020-07-04 10:57:57 +0530

    a

---
+++ yugyntrm/yugyntrm.c
@@ -0,0 +1,81 @@
+/* github.com/031323/yugyntrm */
+
+#include "yugyntrm.h"
+
+#include <malloc.h>
+#include <stdio.h>
+
+#include <math.h>
+
+typedef enum { PRAKRTM, YOGH, DISTRUPM } yugdrma;
+
+typedef struct
+{
+	yugdrma drma;
+	yugsnkya snkya;
+	yugm svym;
+	yugm dksinm;
+} yugrupm;
+
+typedef yugrupm * yugruppdm;
+
+typedef struct
+{
+	yugruppdm pdm;
+	yugsnkya vrima;
+	yugm prtmriktm;
+} smbrnm;
+
+typedef smbrnm * smbrnpdm;
+
+static yugm suddm(const * yugm);
+
+smbrnm nvsmbrnm()
+{
+	smbrnm ynnvm = { 0, 0, 0 };
+	return ynnvm;
+}
+
+int vrdnani = 0;
+
+yugm nvm(smbrnpdm pdm)
+{
+	yugm prtmriktm = pdm->prtmriktm;
+	if(prtmriktm == pdm->vrima) {
+		yugsnkya vrima = prtmriktm ? prtmriktm * VRDNM : 1;
+		yugruppdm nvpdm = malloc(vrima * sizeof(yugrupm));
+		for(yugm ekm = 0; ekm < prtmriktm; ekm++) {
+			nvpdm[ekm] = pdm->pdm[ekm];
+		}
+		for(yugm ekm = prtmriktm; ekm < vrima; ekm ++) {
+			nvpdm[ekm].dksinm = ekm + 1;	
+		}
+		pdm->pdm = nvpdm;
+		pdm->vrima = vrima;
+		vrdnani++;
+		printf("%d vrdnani\n", vrdnani);
+	}
+	pdm->prtmriktm = pdm->pdm[prtmriktm].dksinm;
+	return prtmriktm;
+}
+
+void recnm(smbrnpdm pdm, yugm riktm)
+{
+	pdm->pdm[riktm].dksinm = pdm->prtmriktm;
+	pdm->prtmriktm = riktm;
+}
+
+int main()
+{
+	smbrnm s = nvsmbrnm();
+	int yugani = 10000;
+	for(int i = 0; i < yugani; i++) {
+		yugm p = nvm(&s);
+		s.pdm[p].dksinm = i * 10;
+	}
+	for(int i = 0; i < 10; i++) {
+		printf("%d\n", s.pdm[i].dksinm);
+	}
+	printf("%f\n", log2((float)yugani));
+	return 0;
+}
---
+++ yugyntrm/yugyntrm.h
@@ -0,0 +1,14 @@
+#ifndef YUGYNTRM_H
+#define YUGYNTRM_H
+
+#ifndef yugsnkya
+#define yugsnkya unsigned int
+#endif
+
+#ifndef VRDNM
+#define VRDNM 2
+#endif
+
+typedef yugsnkya yugm;
+
+#endif
